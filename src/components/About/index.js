6d72bfbc386b3c19fd41df2a5d64cdadf76b52ee
Jiyeon31 <zhdzhddlx@gmail.com>
2022-06-07 16:08:13 -0400

    changed about content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,11 +6,7 @@ function About() {
     <section>
       <h1 className="my-2 py-2" id="about">About me</h1>
         <img src={profileImage} className="my-2 round" style={{ width: "25%" }} alt="profile" />
-            <p className="my-2 py-2">I know English, Korean and Mandarin and more! also I'm passionate about software engineering and gaming industry.
-            Have worked at a cosmetic store for 1 year as a sales staff and maintained 80% of income as before covid in 2020.
-            Easily adapt to any environment through sales staff experience.
-            Get along with anyone well and good at coping with any arguable situations through teaching experience.
-            Ready to work as a global worker with proper language skills and international experiences.</p>
+            <p className="my-2 py-2">Passionate about programming industry. Easily adapt to any environment through sales staff experience. Get along with people around me well and good at coping with any arguable situations through teaching experience. Ready to work as a global worker with proper language skills and international experiences.</p>
     </section>
   );
 }
